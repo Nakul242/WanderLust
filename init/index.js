372c154b956125509f2599dace8b5b938e0530e3
Nakul242 <nakuljadav24@gmail.com>
2026-01-16 19:42:18 +0530

    Fix seed data, geometry for Leaflet

--- init/index.js
+++ init/index.js
@@ -1,29 +1,35 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+const User = require("../models/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-    .then(() => {
-        console.log("connectecd to DB");
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+  .then(() => console.log("Connected to DB"))
+  .catch(err => console.log(err));
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
-    await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) => ({
-        ...obj,
-        owner: "696218aa32f4d43aad46e1be", // setting a default owner for all listings
-    }));
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
+  await Listing.deleteMany({});
+
+  const user = await User.findOne();
+
+  if (!user) {
+    console.log("❌ No user found. Please register a user first.");
+    return;
+  }
+
+  const listingsWithOwner = initData.data.map(obj => ({
+    ...obj,
+    owner: user._id
+  }));
+
+  await Listing.insertMany(listingsWithOwner);
+  console.log("✅ Data was initialized with owner");
 };
 
 initDB();
